2d2c1d2069806b60bbb229f1ac33ceb5070df98e
Ivan Fraixedes <ifcdev@gmail.com>
2013-12-24 18:11:50 +0000

    Working in providing more hooks to the module
    I am adding more hooks to the module to be able to use it in the
    different workflows; moreover I will provide to the module a command
    line argument option to only call the action of post build when it will
    be used

--- src/index.js
+++ src/index.js
@@ -5,8 +5,38 @@ var config = require('./config');
 var bustAssetsFiles = require('./bust-assets-files');
 
 var registration = function(mimosaConfig, register) {
+    register(['add', 'update', 'remove'], 'complete', function (mimosaConfig, workflowInfo, next) {
+      //console.log('The arguments are: ', arguments);
+      next();
+    });
+
+    register(['postBuild'], 'afterOptimize', function (mimosaConfig, workflowInfo, next) {
+      //console.log('The arguments in afterOptimize are: ', arguments);
+      next();
+    });
+
+    register(['cleanFile'], 'delete', function (mimosaConfig, workflowInfo, next) {
+      //console.log(workflowInfo);
+      next();
+    });
+
     register(['postBuild'], 'beforePackage', bustAssetsFiles);
 };
+
+
+function _cleanFileOnDelete(mimosaConfig, workflowInfo, next) {
+  var filesRefs = workflowInfo.files;
+
+  if (0 === filesRefs.length) {
+    next();
+    return;
+  }
+
+  filesRefs.forEach(function(fileRef) {
+    //fileRef.outputFileName
+
+  });
+}
 
 module.exports = {
   registration: registration,
